b8c71a68e1fc36007d21dc8d08e4f8b9f48e0653
alpbourne <alpbourne@gmail.com>
2017-11-01 19:57:58 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,6 +1,6 @@
 class CreateCostumeStores < ActiveRecord::Migration[4.2]
   def change
-    create_table :costume_store do |t|
+    create_table :costume_stores do |t|
       t.string :name
       t.float :price
       t.string :size
